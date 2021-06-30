7e01d72b6e122c72569dbcb270a733a4729e163d
DapperBlondie <alirezagharib110@gmail.com>
2021-07-01 00:44:13 +0430

    Postgres functions for applying query over them implemented

--- pkg/repository/dbrepo/postgres.go
+++ pkg/repository/dbrepo/postgres.go
@@ -11,6 +11,7 @@ func (r *PostgresDBRepo) AllUsers() bool {
 	return true
 }
 
+// InsertReservation Insert a reservation in the database
 func (r *PostgresDBRepo) InsertReservation(res models.Reservations) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
@@ -39,17 +40,77 @@ func (r *PostgresDBRepo) InsertReservation(res models.Reservations) (int, error)
 	return newID, nil
 }
 
+// InsertRestrictionRoom Insert a restriction for a reserved room into the database
 func (r *PostgresDBRepo) InsertRestrictionRoom(restrict models.RoomsRestrictions) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*6)
 	defer cancel()
 
-	stmt := `INSERT INTO `
+	stmt := `INSERT INTO room_restrictions 
+    (start_Date,end_date,room_id,reservation_id,restriction_id,created_at,updated_at) VALUES 
+    ($1,$2,$3,$4,$5,$6,$7)`
 
-	_, err := r.DB.ExecContext(ctx, stmt)
+	_, err := r.DB.ExecContext(ctx, stmt,
+		restrict.StartDate,
+		restrict.EndDate,
+		restrict.RoomID,
+		restrict.ReservationID,
+		restrict.RestrictionID,
+		time.Now(),
+		time.Now())
+
 	if err != nil {
 		log.Println("We have some error for inserting restriction data into database : " + err.Error())
 		return err
 	}
 
 	return nil
+}
+
+// SearchAvailabilityByDate search across the availability of the times we have there
+func (r *PostgresDBRepo) SearchAvailabilityByDateByRoomID(start, end time.Time, roomID int) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*6)
+	defer cancel()
+
+	query := `SELECT count(id) FROM room_restrictions WHERE room_id=$1 
+                                          AND $2 < end_date AND $3 > start_date`
+	var numRows int
+
+	err := r.DB.QueryRowContext(ctx, query, roomID, start, end).Scan(&numRows)
+	if err != nil {
+		log.Println("[SearchAvailabilityByDate] Error in getting the number of rows : " + err.Error() + "\n")
+		return -1, err
+	}
+
+	return numRows, nil
+}
+
+//SearchAvailabilityForAllRooms Using for scanning the result of the searching availability of all rooms
+func (r *PostgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Rooms, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*6)
+	defer cancel()
+
+	var rooms []models.Rooms
+
+	query := `SELECT rooms.id, rooms.room_name
+				FROM rooms WHERE rooms.id NOT IN 
+				SELECT room_id FROM room_restrictions WHERE $1 < room_restrictions.end_date 
+				AND  $2 > room_restrictions.start_date`
+
+	rows, err := r.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		log.Println("[SearchAvailabilityForAllRooms] We have an error for getting the information : " + err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		var room models.Rooms
+		err := rows.Scan(&room.ID, room.RoomName)
+		if err != nil {
+			log.Println("[SearchAvailabilityForAllRooms] We have an error for scanning the result : " + err.Error())
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	return rooms, nil
 }
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/DapperBlondie/booking_system/pkg/models"
+import (
+	"github.com/DapperBlondie/booking_system/pkg/models"
+	"time"
+)
 
 // DatabaseRepo Holds all common functionalities that are common between all DBs Drivers
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservations) (int, error)
 	InsertRestrictionRoom(restrict models.RoomsRestrictions) error
+	SearchAvailabilityByDateByRoomID(start, end time.Time, roomID int) (int, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Rooms, error)
 }
